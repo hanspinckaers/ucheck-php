e02bed63ccc1d67932cbb66e2cce93efd478643c
Hans Pinckaers <hans.pinckaers@gmail.com>
2011-08-14 12:29:24 +0200

    add setup.php for real

---
+++ setup.php
@@ -0,0 +1,17 @@
+<?
+## Copyright (c) 2011 by Hans Pinckaers 
+##
+## This work is licensed under the Creative Commons 
+## Attribution-NonCommercial-ShareAlike 3.0 Unported License. 
+## To view a copy of this license, visit 
+## http://creativecommons.org/licenses/by-nc-sa/3.0/ 
+##
+## ucheck-php: https://github.com/HansPinckaers/ucheck-php
+## ucheck-node: https://github.com/HansPinckaers/ucheck-node
+##
+
+$DOCUMENT_ROOT = realpath($_SERVER['DOCUMENT_ROOT'])."/";
+$NODE_SERVER = "http://109.72.92.55:3000/";
+$NODE_FALLBACK = "http://ucheck.nodester.com/";
+
+?>
