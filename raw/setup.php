deb4241a846e063b4f608540bb29cd98c1e5689a
Hans Pinckaers <hans.pinckaers@gmail.com>
2012-07-15 16:07:42 +0200

    Forgot two ";"

--- raw/setup.php
+++ raw/setup.php
@@ -15,8 +15,8 @@ if(!isset($DOCUMENT_ROOT))
     $DOCUMENT_ROOT = realpath($_SERVER['DOCUMENT_ROOT'])."/";
     $USES_UCHECK_API = true;
     $UCHECK_API_SERVER = "http://ucheck.nl/api/";
-    $NODEJS_DIR = "" // path to node.js bin;
-    $NODEJS_SERVERJS_DIR = "" // path to uCheck server.js;
+    $NODEJS_DIR = ""; // path to node.js bin;
+    $NODEJS_SERVERJS_DIR = ""; // path to uCheck server.js;
     
     // here you can tweak the CURL used to scrape
     function req($url, $post_str, $cookiefile)
