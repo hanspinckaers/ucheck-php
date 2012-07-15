f84dae9dfb208a2538c7ec9188d8846f6bf531b8
Hans Pinckaers <hans.pinckaers@gmail.com>
2012-07-15 15:54:13 +0200

    Additional setup options.

--- raw/setup.php
+++ raw/setup.php
@@ -10,10 +10,40 @@
 ## ucheck-node: https://github.com/HansPinckaers/ucheck-node
 ##
 
-$DOCUMENT_ROOT = realpath($_SERVER['DOCUMENT_ROOT'])."/";
-$NODE_SERVER = "http://ec2-79-125-78-169.eu-west-1.compute.amazonaws.com:3000/";
-$NODE_FALLBACK = "http://ec2-79-125-78-169.eu-west-1.compute.amazonaws.com:3000/";
-
-//$NODE_FALLBACK = "http://ufix.nodester.com/";
+if(!isset($DOCUMENT_ROOT))
+{
+    $DOCUMENT_ROOT = realpath($_SERVER['DOCUMENT_ROOT'])."/";
+    $USES_UCHECK_API = true;
+    $UCHECK_API_SERVER = "http://ucheck.nl/api/";
+    $NODEJS_DIR = "" // path to node.js bin;
+    $NODEJS_SERVERJS_DIR = "" // path to uCheck server.js;
+    
+    // here you can tweak the CURL used to scrape
+    function req($url, $post_str, $cookiefile)
+    {
+        $ch = curl_init();
+    
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; nl-nl) AppleWebKit/533.18.1 (KHTML, like Gecko) Version/5.0.2 Safari/533.18.5");
+        curl_setopt($ch, CURLOPT_URL, $url);
+    
+        curl_setopt($ch, CURLOPT_POST, substr_count($post_str, "&") + 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_str);
+    
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);  
+    
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookiefile);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $cookiefile);
+    
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    
+        $html = curl_exec($ch);
+    
+        unset($ch);
+    
+        return $html;
+    }
+}
 
 ?>
