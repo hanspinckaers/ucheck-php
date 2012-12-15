81f297c814d72506ce4064541bae735535ec1b54
Hans Pinckaers <hans.pinckaers@gmail.com>
2012-12-15 19:55:25 +0100

    Uitschrijven update.

--- raw/uitschrijven.php
+++ raw/uitschrijven.php
@@ -50,18 +50,25 @@ foreach($raw_vakken as $raw_vak)
 
 $all_matches = array();
 
-foreach($terms as $term)
-{
+// $term = '10';
+// foreach($terms as $term)
+// {
 	//url-ify the data for the POST
 	foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
 	rtrim($fields_string,'&');
 	
+	$term = '10';
+	
 	$y = ($year == '11') ? '10' : '21';
+	// $y = '21';
 	$url = 'https://usis.leidenuniv.nl/psc/S040PRD/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_DROP.GBL?Page=SSR_SSENRL_DROP&Action=A&ACAD_CAREER='.$term.'&INSTITUTION=LEI01&STRM=21'.$y;
 	$html = req($url, $fields_string, $cookiefile);
 	
+	
 	$url = 'https://usis.leidenuniv.nl/psc/S040PRD/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_DROP.GBL';
-	$post ="ICType=Panel&ICElementNum=0&ICAction=DERIVED_REGFRM1_LINK_DROP_ENRL&ICXPos=0&ICYPos=0&ResponsetoDiffFrame=-1&TargetFrameName=None&ICFocus=&ICSaveWarningFilter=0&ICChanged=-1&ICResubmit=1&ICSID=VWJH4XuwW5DikH2UFpQZhPt5WEK5u5nlWMeLNWNWIJI%3D&ICModalWidget=0&ICZoomGrid=0&ICZoomGridRt=0&ICModalLongClosed=&ICActionPrompt=false&ICFind=&ICAddCount=&DERIVED_SSTSNAV_SSTS_MAIN_GOTO$5$=0100&DERIVED_SSTSNAV_SSTS_MAIN_GOTO$105$=0100";
+	$post ="ICType=Panel&ICElementNum=0&ICAction=DERIVED_REGFRM1_LINK_DROP_ENRL&ICXPos=0&ICYPos=0&ResponsetoDiffFrame=-1&TargetFrameName=None&ICFocus=&ICSaveWarningFilter=0&ICChanged=-1&ICResubmit=1&ICSID=C4Dv5nH55Q7cctCCvCuF0eADRvXX2QiwGR2hpzep140%3D&ICModalWidget=0&ICZoomGrid=0&ICZoomGridRt=0&ICModalLongClosed=&ICActionPrompt=false&ICFind=&ICAddCount=&%23ICDataLang=ENG&DERIVED_SSTSNAV_SSTS_MAIN_GOTO%244%24=0100&DERIVED_SSTSNAV_SSTS_MAIN_GOTO%24109%24=0100";
+	// $post ="ICType=Panel&ICElementNum=0&ICAction=DERIVED_REGFRM1_LINK_DROP_ENRL&ICXPos=0&ICYPos=0&ResponsetoDiffFrame=-1&TargetFrameName=None&ICFocus=&ICSaveWarningFilter=0&ICChanged=-1&ICResubmit=1&ICSID=C4Dv5nH55Q7cctCCvCuF0eADRvXX2QiwGR2hpzep140%3D&ICModalWidget=0&ICZoomGrid=0&ICZoomGridRt=0&ICModalLongClosed=&ICActionPrompt=false&ICFind=&ICAddCount=&DERIVED_SSTSNAV_SSTS_MAIN_GOTO$4$=0100&DERIVED_SSTSNAV_SSTS_MAIN_GOTO$109$=0100";
+	// $post ="ICType=Panel&ICElementNum=0&ICAction=DERIVED_REGFRM1_LINK_DROP_ENRL&ICXPos=0&ICYPos=0&ResponsetoDiffFrame=-1&TargetFrameName=None&ICFocus=&ICSaveWarningFilter=0&ICChanged=-1&ICResubmit=1&ICSID=VWJH4XuwW5DikH2UFpQZhPt5WEK5u5nlWMeLNWNWIJI%3D&ICModalWidget=0&ICZoomGrid=0&ICZoomGridRt=0&ICModalLongClosed=&ICActionPrompt=false&ICFind=&ICAddCount=&DERIVED_SSTSNAV_SSTS_MAIN_GOTO$4$=0100&DERIVED_SSTSNAV_SSTS_MAIN_GOTO$109$=0100";
 	
 	$vakken = explode(",", $_GET['q']);
 	
@@ -87,14 +94,16 @@ foreach($terms as $term)
 		// error found
 	} else {
 		$url = 'https://usis.leidenuniv.nl/psc/S040PRD/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_DROP.GBL';
-		$post_str = "Type=Panel&ICElementNum=0&ICAction=DERIVED_REGFRM1_SSR_PB_SUBMIT&ICXPos=0&ICYPos=1267&ICFocus=&ICSaveWarningFilter=0&ICChanged=-1&ICResubmit=0&ICSID=qcNpMdLKb6My&DERIVED_SSTSNAV_SSTS_MAIN_GOTO$4$=9999&DERIVED_SSTSNAV_SSTS_MAIN_GOTO$100$=9999";
+		// $post_str = "Type=Panel&ICElementNum=0&ICAction=DERIVED_REGFRM1_SSR_PB_SUBMIT&ICXPos=0&ICYPos=0&ResponsetoDiffFrame=-1&TargetFrameName=None&ICFocus=&ICSaveWarningFilter=0&ICChanged=-1&ICResubmit=0&ICSID=C4Dv5nH55Q7cctCCvCuF0eADRvXX2QiwGR2hpzep140%3D&ICModalWidget=0&ICZoomGrid=0&ICZoomGridRt=0&ICModalLongClosed=&ICActionPrompt=false&ICFind=&ICAddCount=&#ICDataLang=ENG&DERIVED_SSTSNAV_SSTS_MAIN_GOTO$4$=0100&DERIVED_SSTSNAV_SSTS_MAIN_GOTO$65$=0100
+		$post_str = "ICType=Panel&ICElementNum=0&ICAction=DERIVED_REGFRM1_SSR_PB_SUBMIT&ICXPos=0&ICYPos=0&ResponsetoDiffFrame=-1&TargetFrameName=None&ICFocus=&ICSaveWarningFilter=0&ICChanged=-1&ICResubmit=1&ICSID=C4Dv5nH55Q7cctCCvCuF0eADRvXX2QiwGR2hpzep140%3D&ICModalWidget=0&ICZoomGrid=0&ICZoomGridRt=0&ICModalLongClosed=&ICActionPrompt=false&ICFind=&ICAddCount=&%23ICDataLang=ENG&DERIVED_SSTSNAV_SSTS_MAIN_GOTO%244%24=0100&DERIVED_SSTSNAV_SSTS_MAIN_GOTO%2465%24=0100";
+		// $post_str = "Type=Panel&ICElementNum=0&ICAction=DERIVED_REGFRM1_SSR_PB_SUBMIT&ICXPos=0&ICYPos=1267&ICFocus=&ICSaveWarningFilter=0&ICChanged=-1&ICResubmit=0&ICSID=qcNpMdLKb6My&DERIVED_SSTSNAV_SSTS_MAIN_GOTO$4$=0100&DERIVED_SSTSNAV_SSTS_MAIN_GOTO$65$=0100";
 		$result = req($url, $post_str, $cookiefile);
 						
 		preg_match_all("/<B>(.*)./", $result, $matches_term);
 		$all_matches = array_merge($all_matches, $matches_term);
 	}	
 	
-}
+// }
 	
 unlink($cookiefile);
 
